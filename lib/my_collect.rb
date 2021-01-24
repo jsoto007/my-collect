4135e79d642298ac7bcae028e0f7af734e242a0e
Jose Soto <jsoto7087@gmail.com>
2021-01-24 12:44:39 -0500

    commit tree 05437f8f42c1eb2c847c01554711d2524d9f52ff

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
 
+# def my_collect(array)
 
+#         i = 0
+#         # my_each do |arr|
+#         while i < array.length
+#           yield(array[i])
+#           i += 1
+#         end 
+#         array
+#       end
+
+#       require 'pry'
+
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
